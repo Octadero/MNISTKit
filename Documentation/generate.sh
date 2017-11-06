43f98fc179f3ae9feadf7a55c93475027e8d4de8
Volodymyr Pavliukevych <volodymyr@octadero.com>
2017-11-06 21:51:48 +0200

    Updated Documentation.

--- Documentation/generate.sh
+++ Documentation/generate.sh
@@ -5,4 +5,6 @@ sourcekitten doc --spm-module MNISTKit > Documentation/MNISTKit.json
 jazzy --config Documentation/MNISTKit.yaml
 
 rm Documentation/MNISTKit.json
-
+# Add access bin/gsutil iam ch allUsers:objectViewer gs://api.octadero.com
+# Upload on webserver
+# /server/repository/google-cloud-sdk/bin/gsutil -m cp -r Documentation/MNISTKit gs://api.octadero.com
